74d347e5820d1a5236dabd160ca49e4bec8b2b8e
Jens <git@ansorg-web.de>
2018-05-15 22:09:12 +0200

    fixed footer

--- ja-simple/php/style.php
+++ ja-simple/php/style.php
@@ -2,4 +2,5 @@
 
 require_once('view/theme/redbasic/php/style.php');
 
+echo @file_get_contents('view/theme/ja-simple/css/style.css');
 echo @file_get_contents('view/theme/ja-simple/css/minimal.css');
