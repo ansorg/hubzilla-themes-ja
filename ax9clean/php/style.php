f95cb26953552fe2f74d2b7d5b8e2741321a6420
Jens <git@ansorg-web.de>
2018-05-17 21:05:40 +0200

    clean theme part 1

---
+++ ax9clean/php/style.php
@@ -0,0 +1,5 @@
+<?php
+
+require_once('view/theme/redbasic/php/style.php');
+
+echo @file_get_contents('view/theme/ax9clean/css/style.css');
---
+++ ax9clean/php/theme.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *   * Name: ax9clean
+ *   * Description: A simple theme based on redbasic
+ *   * Version: 0.1
+ *   * MinVersion: 2.3.4
+ *   * MaxVersion: 4.0
+ *   * Author: Jens Ansorg
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function ax9clean_init(&$App) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
