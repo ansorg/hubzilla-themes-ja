b71521ac9762fb88fd8b194aa38b52afa417308b
Jens <git@ansorg-web.de>
2018-05-17 16:43:41 +0200

    init function not called? need own theme_init?

--- ja-simple/php/theme_init.php
+++ ja-simple/php/theme_init.php
@@ -1,5 +1,5 @@
 <?php
-/*
+
 head_add_css('/library/fork-awesome/css/fork-awesome.min.css');
 head_add_css('/library/bootstrap/css/bootstrap.min.css');
 head_add_css('/library/bootstrap-tagsinput/bootstrap-tagsinput.css');
@@ -12,4 +12,3 @@ head_add_js('/library/bootbox/bootbox.min.js');
 head_add_js('/library/bootstrap-tagsinput/bootstrap-tagsinput.js');
 head_add_js('/library/datetimepicker/jquery.datetimepicker.js');
 head_add_js('/library/bootstrap-colorpicker/dist/js/bootstrap-colorpicker.js');
-*/
