5a6cbca8a71f1d6223cab8f8f138d21338a9c40a
Jens <git@ansorg-web.de>
2018-10-19 10:55:12 +0200

    new files

---
+++ ax9reset/php/style.php
@@ -0,0 +1,6 @@
+<?php
+
+require_once('view/theme/redbasic/php/style.php');
+
+echo @file_get_contents('view/theme/ax9reset/css/style.css');
+echo @file_get_contents('view/theme/ax9reset/css/minimal.css');
---
+++ ax9reset/php/theme.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ *   * Name: Simple
+ *   * Description: A simple theme based on redbasic
+ *   * Version: 0.1
+ *   * MinVersion: 2.3.4
+ *   * MaxVersion: 4.0
+ *   * Author: Jens Ansorg
+ *   * Compat: Hubzilla [*]
+ *
+ */
+
+function ax9reset_init(&$App) {
+
+    App::$theme_info['extends'] = 'redbasic';
+
+
+}
